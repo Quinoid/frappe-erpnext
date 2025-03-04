eb163b3f6b92f38324a17cb6cae50d00677c8697
Arya <Arya-Dilipan>
2025-03-04 09:01:22 +0530

    Welcome email update

--- erpnext/setup/utils.py
+++ erpnext/setup/utils.py
@@ -163,5 +163,5 @@ def insert_record(records):
 
 def welcome_email():
 	site_name = get_default_company() or "ERPNext"
-	title = _("Welcome to {0}").format(site_name)
+	title = _("Welcome to {0} CRM").format(site_name)
 	return title
